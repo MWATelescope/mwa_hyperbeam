9aea18ac724f8cdf03d085bad49e3b1a021ce3fb
Christopher H. Jordan <christopherjordan87@gmail.com>
2023-01-14 21:55:09 +0800

    Put Rust examples alongside the other languages.

--- examples/fee_cuda.rs
+++ examples/fee_cuda.rs
@@ -5,7 +5,7 @@
 //! Example code using hyperbeam's CUDA with Rust.
 //!
 //! Build and run with something like:
-//! `cargo run --release --features=cuda-single --example fee_cuda -- mwa_full_embedded_element_pattern.h5 1000000`
+//! `cargo run --release --features=cuda-single --example fee_cuda -- 1000000 mwa_full_embedded_element_pattern.h5`
 //!
 //! If you want to use hyperbeam in your own Rust crate, then check out the latest
 //! version on crates.io:
@@ -14,43 +14,28 @@
 
 use std::f64::consts::{FRAC_PI_2, PI};
 
-use clap::Parser;
 use mwa_hyperbeam::{fee::FEEBeam, AzEl, CudaFloat, Jones};
 use ndarray::prelude::*;
 
-#[derive(Parser, Debug)]
-#[clap(allow_negative_numbers = true)]
-struct Args {
-    /// Path to the HDF5 file.
-    #[clap(short, long, parse(from_os_str))]
-    hdf5_file: Option<std::path::PathBuf>,
-
-    /// If provided, use this latitude for the parallactic-angle correction.
-    #[clap(short, long)]
-    latitude_rad: Option<f64>,
-
-    /// The number of directions to run.
-    #[clap()]
-    num_directions: usize,
-}
-
-fn main() -> Result<(), anyhow::Error> {
-    let args = Args::parse();
-    if args.num_directions == 0 {
-        eprintln!("num_directions cannot be 0.");
-        std::process::exit(1);
-    }
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut args = std::env::args().skip(1);
+    let num_directions: usize = args
+        .next()
+        .expect("number of directions supplied")
+        .parse()
+        .expect("number of directions is a number");
+    let beam_file = args.next();
+    // If we were given a file, use it. Otherwise, fall back on MWA_BEAM_FILE.
+    let beam = match beam_file {
+        Some(f) => FEEBeam::new(f)?,
+        None => FEEBeam::new_from_env()?,
+    };
 
     println!(
         "CUDA float precision is {} bits",
         std::mem::size_of::<CudaFloat>() * 8
     );
 
-    // If we were given a file, use it. Otherwise, fall back on MWA_BEAM_FILE.
-    let beam = match args.hdf5_file {
-        Some(f) => FEEBeam::new(f)?,
-        None => FEEBeam::new_from_env()?,
-    };
     // Set up our "CUDA beam".
     let freqs_hz = [150e6 as u32, 200e6 as _];
     // Delays and amps correspond to dipoles in the "M&C order". See
@@ -61,21 +46,23 @@ fn main() -> Result<(), anyhow::Error> {
     let num_tiles = 1;
     let delays = Array2::zeros((num_tiles, 16));
     let amps = Array2::ones((num_tiles, 16));
-    let norm_to_zenith = false;
+    let norm_to_zenith = true;
+
     let cuda_beam =
         unsafe { beam.cuda_prepare(&freqs_hz, delays.view(), amps.view(), norm_to_zenith)? };
 
     // Set up the directions to test. The type depends on the CUDA precision.
-    let mut azels = Vec::with_capacity(args.num_directions);
-    for i in 0..args.num_directions {
-        let az = 0.4 + 0.3 * PI * (i / args.num_directions) as f64;
-        let za = 0.3 + 0.4 * FRAC_PI_2 * (i / args.num_directions) as f64;
+    let mut azels = Vec::with_capacity(num_directions);
+    for i in 0..num_directions {
+        let az = 0.4 + 0.3 * PI * (i / num_directions) as f64;
+        let za = 0.3 + 0.4 * FRAC_PI_2 * (i / num_directions) as f64;
         azels.push(AzEl::new(az, FRAC_PI_2 - za));
     }
-    let iau_order = true;
 
     // Call hyperbeam CUDA code.
-    let jones = cuda_beam.calc_jones(&azels, args.latitude_rad, iau_order)?;
+    let array_latitude_rad = Some(-0.4660608448386394); // MWA
+    let iau_order = true;
+    let jones = cuda_beam.calc_jones(&azels, array_latitude_rad, iau_order)?;
     println!("The first Jones matrix:");
     // This works, but the formatting for this isn't very pretty.
     // println!("{}", jones[(0, 0, 0)]);
@@ -91,7 +78,7 @@ fn main() -> Result<(), anyhow::Error> {
     // fun. `beam.calc_jones` does the parallactic correction,
     // `beam.calc_jones_eng` does not. Regenerate the direction at double
     // precision for CPU usage.
-    let azel = AzEl::new(azels[0].az as f64, azels[0].el as f64);
+    let azel = AzEl::new(azels[0].az, azels[0].el);
 
     let jones_cpu = beam.calc_jones(
         azel,
@@ -99,7 +86,7 @@ fn main() -> Result<(), anyhow::Error> {
         delays.slice(s![0, ..]).as_slice().unwrap(),
         amps.slice(s![0, ..]).as_slice().unwrap(),
         norm_to_zenith,
-        args.latitude_rad,
+        array_latitude_rad,
         iau_order,
     )?;
 
